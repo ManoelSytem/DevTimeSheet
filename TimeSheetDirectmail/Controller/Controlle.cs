5c7817f7aded684677d6daf58d5c7a51fe88b8d0
Manoel Neto <manoelcontatosi@gmail.com>
2019-03-14 16:10:00 -0300

    Projeto TimeSheet - Direct email

---
+++ TimeSheetDirectmail/Controller/Controlle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeSheetDirectmail.Model;
+using TimeSheetDirectmail.Repository;
+
+namespace TimeSheetDirectmail.Controller
+{
+    public class Controlle
+    {
+        private ProtheusRepository _repositoryProthues;
+       
+        public Controlle(ProtheusRepository repositoryProthues)
+        {
+            _repositoryProthues = repositoryProthues;
+        }
+
+
+        public void EnviarEmail()
+        {
+          
+           Notificacao notificacao = new Notificacao();
+           Configuracao config = new Configuracao();
+           config  =  _repositoryProthues.ObterConfiguracao();
+           notificacao.EnviarEmail("manoelcontatosi@gmail.com", "Prezado, teste envio e-mail", config.AssuntoEmail);
+
+            if (config.ValidaDataEnvioEmail())
+            {
+               if(config.Frequencia_email == 1)
+                {
+                    notificacao.EnviarEmail("manoelcontatosi@gmail.com" , "Prezado, teste envio e-mail", config.AssuntoEmail);
+                }
+                else
+                { 
+                    if(config.Qtddiadatafechamento == DateTime.Now.Day | DateTime.Now.DayOfWeek == DayOfWeek.Sunday
+                    &&
+                    DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+                    {
+                        notificacao.EnviarEmail("manoelcontatosi@gmail.com", "Prezado, teste envio e-mail", config.AssuntoEmail);
+                    }
+                }
+
+            }    
+           
+        }
+    }
+
+
+}
---
+++ TimeSheetDirectmail/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeSheetDirectmail.Controller;
+using TimeSheetDirectmail.Repository;
+
+namespace TimeSheetDirectmail
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtheusRepository repository = new ProtheusRepository();
+            Controlle novo = new Controlle(repository);
+            novo.EnviarEmail();
+            Console.WriteLine("TimeSheet Envindo em de fechamento pendentes aguarde...");
+            Console.ReadKey();
+        }
+    }
+}
